c07ea874f02286272012b8ee1c56c3044c913e67
Javier Rios <javiererios1@gmail.com>
2024-06-19 23:11:20 -0500

    Initial Commit

---
+++ ClickaDo/ClickaDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClickaDoApp.swift
+//  ClickaDo
+//
+//  Created by Javier R on 6/19/24.
+//
+
+import SwiftUI
+
+@main
+struct ClickaDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClickaDo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ClickaDo
+//
+//  Created by Javier R on 6/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
